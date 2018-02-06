e051a8420080e1be30936e599d11bfa06cf2e1ef
Raiet Intraub <raietsaeed@gmail.com>
2018-02-06 19:38:51 +0000

    sql-databases-and-text-editors sandbox

---
+++ sql-databases-and-text-editors.sql
@@ -0,0 +1,15 @@
+
+-- 01_create_cats_table.sql
+/*CREATE TABLE cats (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+); */
+
+
+-- 02_add_column_to_cats.sql
+  ALTER TABLE cats ADD COLUMN breed TEXT;
+
+
+
+-- https://www.techonthenet.com/sqlite/comments.php
